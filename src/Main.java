bc54f7ea10ce470723d3fd0311e9d3cd32ffa0c9
Iván <ivajuamar2@alu.edu.gva.es>
2022-12-16 22:34:40 +0100

    [MOD] Segundo commit - cambio2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         for (int i = 0; i <= 20; i++){
             System.out.print(i + ",");
             System.out.println("Cambio1");
+            System.out.println("Cambio2");
         }
     }
 }
